3dbac665a06f0d08f4d1288d367432814b3700cc
darkclone <darkclone@users.noreply.github.com>
2016-01-19 15:14:57 -0500

    Update massdiff1.sh

--- massdiff1.sh
+++ massdiff1.sh
@@ -1,14 +1,13 @@
-filename = $1
-dir = $(dirname $0)
-filename = dir/filename
-for f in ls
-do
-    cf = dir/f
-    difference = diff cf filename
-    if [difference == 0]
+filename=$1
+dir=$(dirname $0)
+filename=dir/filename
+for f in *; do
+    cf=dir/f
+    difference=$(diff cf filename)
+    if [ $difference == 0 ]
     then
         echo "$f and $filename are the same"
     else
-        difference >> dir/results.log
+        difference >> dir/log.txt
     fi
 done
