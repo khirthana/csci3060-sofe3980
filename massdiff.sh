5244daf8183bb84daea343e9b057c4d6fc9a2641
khirthana <khirthana.subramanian@uoit.net>
2016-01-23 17:57:08 -0500

    updated and working

--- massdiff.sh
+++ massdiff.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+filename=$1
 
-result=
-
-echo "F and File are the same"
-
-echo $result > results.log
+for f in `ls -a *.*`
+do
+ if diff -q "$f" "$filename"; then
+    echo "$f and $filename are the same"
+ else
+    diff -q "$f" "$filename" >> log.txt
+fi
+done
