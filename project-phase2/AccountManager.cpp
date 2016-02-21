0a0f0a0de1cfee085c6804ceb9025df5c5e90f15
darkclone <darkclone@users.noreply.github.com>
2016-02-20 22:12:51 -0500

    Added files via upload
    Basic layout in my mind, utilities should house all rough stuff like reading and writing of the account formats and everything that is reused several times as a function (keep everything modular and simple). It is somewhat similar to what was shown in the uml picture sent to me, basically its using controller as a more of a powerhouse for everything, and splitting account into a account "processor" and account as a data structure style.

---
+++ project-phase2/AccountManager.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+
+class AccountManager
+{
+public:
+	void login(){
+		//an example usage of the utilities
+		string something = Utilities.prompt("Kind of session",["standard","admin"]);
+		string name =  Utilities.prompt("Account holders name",reg="\u\l+ \u\l+") 
+	}
+	void withdrawl(){
+		
+	}
+	void transfer(){
+		
+	}
+	void paybill(){
+		
+	}
+	void deposit(){
+		
+	}
+	void createAcc(){
+		
+	}
+	void deleteAcc(){
+		
+	}
+	void enable(){
+		
+	}
+	void disable(){
+		
+	}
+}
+
