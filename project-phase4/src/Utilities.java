41d5dbe50be8e83695bb5f7986ce0ebd79e85f2d
khirthana <khirthana.subramanian@uoit.net>
2016-03-17 01:03:09 -0400

    Delete Utilities.java

--- project-phase4/src/Utilities.java
+++
@@ -1,9 +0,0 @@
-/*
-authors: Khirthana Subramanian - 100453865
-         Jaina Patel - 100523188
-				 James Morrison - 100524362
-This class is meant to contain simplified and modular usages of common functions
- */
-
-
-import account.java
