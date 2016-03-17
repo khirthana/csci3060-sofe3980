32947ac7eeea4e7427013b8f29c8fb71f3b9e05a
khirthana <khirthana.subramanian@uoit.net>
2016-03-17 01:02:57 -0400

    Delete Account.java

--- project-phase4/src/Account.java
+++
@@ -1,6 +0,0 @@
-/*
-authors: Khirthana Subramanian - 100453865
-         Jaina Patel - 100523188
-		 James Morrison - 100524362
-This class contains functions to retrieve and store account information
-*/
