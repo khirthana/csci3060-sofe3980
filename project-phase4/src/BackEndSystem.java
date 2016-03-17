2c0406fa7e3cbeaac1a2c0fd5b93ae76984fa93e
khirthana <khirthana.subramanian@uoit.net>
2016-03-17 01:03:04 -0400

    Delete BackEndSystem.java

--- project-phase4/src/BackEndSystem.java
+++
@@ -1,9 +0,0 @@
-/*
-authors: Khirthana Subramanian - 100453865
-         Jaina Patel - 100523188
-				 James Morrison - 100524362
-*This class contains functions to compute backend transactions on accounts
-*/
-
-import utilities.java
-import account.java
